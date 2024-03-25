75537f39664261f5cef33de094c69b5a7f66c3fe
Murkyskate <pratyush.kumar300@gmail.com>
2024-03-26 01:42:06 +0530

    update

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page content.</p>
+      <link href='https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css' rel='stylesheet' />
+    </div>
+  );
+};
+
+export default Home;
